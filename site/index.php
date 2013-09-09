1125fd32c43238b14132d0eff18e6b884353f3be
Valeriu Tihai <valeriu@tihai.md>
2013-09-09 18:53:05 -0400

    css html

---
+++ site/index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>index</title>
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" href="images/favicon.ico" />
+	<link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="css/flexslider.css" />
+		<link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
+	<link rel="author" href="https://plus.google.com/106077873424472551858/posts"/>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Quicksand:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript" src="js/html5.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/base.js"></script>
+	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+</head>
+<body>
+	<header class="centercontent">
+		<div class="logo fleft">
+			<a href="index.php" title="Honda Accord 2014">
+				<img src="images/logo-honda-accord.png" alt="Honda Accord 2014">
+			</a>
+		</div>
+		<div class="essai fright">
+			<a href="essai.php">Demander un essai routier</a>
+		</div>
+		<div class="clear"></div>
+	</header>
+	<nav>
+		<ul class="centercontent">
+			<li><a href="">Survol</a></li>
+			<li><a href="">Extérieur</a></li>
+			<li><a href="">Intérieur</a></li>
+			<li><a href="">Sécurité</a></li>
+			<li><a href="">Spécifications</a></li>
+			<li><a href="">Garantie</a></li>
+			<li><a href="">Contact</a></li>
+		</ul>
+		<div class="clear"></div>
+	</nav>
+    <!-- Slider  -->     
+	<div class="flexslider centercontent">
+		<ul class="slides">
+		<li class="sliderli">
+			<img src="images/slider/01.jpg" />
+			<div class="centercontentslider">
+				1 Le Lorem Ipsum est simplement du faux texte
+			</div>
+		</li>
+		<li class="sliderli">
+			<img src="images/slider/02.jpg" />
+			<div class="centercontentslider">
+				2 Le Lorem Ipsum est simplement du faux texte
+			</div>
+		</li>
+			<li class="sliderli">
+			<img src="images/slider/03.jpg" />
+			<div class="centercontentslider">
+				3 Le Lorem Ipsum est simplement du faux texte
+			</div>
+		</li>
+			<li class="sliderli">
+			<img src="images/slider/04.jpg" />
+			<div class="centercontentslider">
+				4 Le Lorem Ipsum est simplement du faux texte
+			</div>
+		</li>
+			<li class="sliderli">
+			<img src="images/slider/05.jpg" />
+			<div class="centercontentslider">
+				5 Le Lorem Ipsum est simplement du faux texte
+			</div>
+		</li>
+		</ul>
+	</div>
+    <footer class="centercontent">
+    	<div class="fhonda fleft">
+    		<div class="titre">Famille Honda</div>
+    		<ul>
+				<li><a href="/fit">Fit</a></li>
+				<li><a href="/civic_coupe_si">Civic Coupe Si</a></li>
+				<li><a href="/civic_sedan_si">Civic Sedan Si</a></li>
+				<li><a href="/civic_coupe">Civic Coupe</a></li>
+				<li><a href="/civic_sedan">Civic Sedan</a></li>
+				<li><a href="/civic_hybrid">Civic Hybrid</a></li>
+				<li><a href="/insight">Insight</a></li>
+				<li><a href="/crz">CR-Z</a></li>
+				<li><a href="/accord_coupe">Accord Coupe</a></li>
+				<li><a href="/accord_sedan">Accord Sedan</a></li>
+				<li><a href="/crv">CR-V</a></li>
+				<li><a href="/crosstour">Crosstour</a></li>
+				<li><a href="/pilot">Pilot</a></li>
+				<li><a href="/odyssey">Odyssey</a></li>
+				<li><a href="/ridgeline">Ridgeline</a></li>
+			</ul>
+    	</div>
+    	<div class="social-icons fright">
+    		<div class="titre">Rester connecté</div>
+    	</div>
+    	<div class="clear"></div>
+
+    	<div class="designby fleft">
+    		FAIT <span>AVEC</span> À MONTRÉAL par <a href="http://valeriu.tihai.md" title="Architecte et Integrateur Web">Valeriu Tihai</a>
+    	</div>
+    	<div class="copyright fright">
+    		© Honda 2013 Tous droits réservés 
+    	</div>
+    	<div class="clear"></div>
+    </footer>
+</body>
+</html>
---
+++ site/js/base.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+	"use strict";
+		/* Flex Slider Teaser */
+	jQuery(window).load(function() {
+		jQuery('.flexslider').flexslider({
+			animation: "slide",
+			animationLoop: true, 
+			controlNav: false
+		});
+	});
+});
