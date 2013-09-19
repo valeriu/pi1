2cef48124758224cbbb1a265e8f0fe6f83c2012e
Valeriu Tihai <valeriu@tihai.md>
2013-09-19 15:25:01 -0400

    delete value from inputs

--- site/essaiC.php
+++ site/essaiC.php
@@ -13,19 +13,19 @@
 				<legend>Information de contact</legend>
 
 				<label for="nom">Nom</label>
-				<input type="text" name="nom" id="nom" value="">
+				<input type="text" name="nom" id="nom" value="Valeriu">
 
 				<label for="prenom">Prénom</label>
-				<input type="text" name="prenom" id="prenom" value="">
+				<input type="text" name="prenom" id="prenom" value="Tihai">
 
 				<label for="courriel">Courriel</label>
-				<input type="text" name="courriel" id="courriel" value="">
+				<input type="text" name="courriel" id="courriel" value="valeriu@tihai.md">
 
 				<label for="telephone">Téléphone</label>
-				<input type="text" name="telephone" id="telephone" value="">
+				<input type="text" name="telephone" id="telephone" value="4389901929">
 				
 				<label for="remarques">Votre message</label>
-				<textarea name="remarques" id="remarques" cols="30" rows="10"></textarea>
+				<textarea name="remarques" id="remarques" cols="30" rows="10">Un text ceva</textarea>
 
 				<input type="submit" value="Soumettre" />
 
